9243cc3d9ac88de49deb68f720659251dd178671
timothyfrankdavies <timothyfrank.davies@ligo.org>
2022-06-21 18:20:00 +1000

    Fix ring_index Cast the modulus to ptrdiff_t to fix cases where `ind < 0` Handle the case where `ind = -n * len` Use an assertion to clarify the function contract `len > 0` Add manually run tests pending a c test framework

---
+++ gstlal-spiir/testing/tests/test_ring_index.c
@@ -0,0 +1,41 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stddef.h>
+
+// FIXME: Replace with a proper testing framework for c and cuda
+// NOTE: This function runs as device code in a cuda kernel (postcoh_kernel.cu)
+//      assert is supported by devices with compute capability 2.x and higher
+static inline size_t ring_index(ptrdiff_t ind, ptrdiff_t len) {
+    assert(len > 0);
+    return ind % len + (ind % len < 0) * len;
+}
+
+int main() {
+    assert(ring_index(0,1) == 0);
+    assert(ring_index(0,10) == 0);
+
+    assert(ring_index(1,1) == 0);
+    assert(ring_index(2,2) == 0);
+    assert(ring_index(4,2) == 0);
+    assert(ring_index(-2,2) == 0);
+    assert(ring_index(-4,2) == 0);
+
+    assert(ring_index(1,2) == 1);
+    assert(ring_index(3,10) == 3);
+    assert(ring_index(11,10) == 1);
+    assert(ring_index(21,10) == 1);
+    
+    assert(ring_index(-1,2) == 1);
+    assert(ring_index(-3,10) == 7);
+    assert(ring_index(-11,10) == 9);
+    assert(ring_index(-21,10) == 9);
+
+    // The assertion itself is not testable, and it shouldn't be tested.
+    // Ideally, it would be replaced with tests on the functions that call it
+    // while tests on ring_index make the code contract explicit.
+    // See https://blog.odd-e.com/basvodde/2011/01/c-assert-and-unit-tests.html
+
+    printf("ALL YOUR TESTS PASSED! CONGRATULATIONS! HAVE A COOKIE!\n");
+
+    return 0;
+}
