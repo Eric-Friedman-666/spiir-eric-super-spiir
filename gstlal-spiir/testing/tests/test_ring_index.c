1c1fd912aa5bb49ce522a841671c039e034972a5
timothyfrankdavies <timothyfrank.davies@ligo.org>
2022-06-22 15:54:38 +1000

    Rerun git-clang

--- gstlal-spiir/testing/tests/test_ring_index.c
+++ gstlal-spiir/testing/tests/test_ring_index.c
@@ -1,6 +1,6 @@
 #include <assert.h>
-#include <stdio.h>
 #include <stddef.h>
+#include <stdio.h>
 
 // FIXME: Replace with a proper testing framework for c and cuda, see #38
 // NOTE: This function runs as device code in a cuda kernel (postcoh_kernel.cu)
@@ -11,24 +11,24 @@ static inline size_t ring_index(ptrdiff_t ind, ptrdiff_t len) {
 }
 
 int main() {
-    assert(ring_index(0,1) == 0);
-    assert(ring_index(0,10) == 0);
+    assert(ring_index(0, 1) == 0);
+    assert(ring_index(0, 10) == 0);
 
-    assert(ring_index(1,1) == 0);
-    assert(ring_index(2,2) == 0);
-    assert(ring_index(4,2) == 0);
-    assert(ring_index(-2,2) == 0);
-    assert(ring_index(-4,2) == 0);
+    assert(ring_index(1, 1) == 0);
+    assert(ring_index(2, 2) == 0);
+    assert(ring_index(4, 2) == 0);
+    assert(ring_index(-2, 2) == 0);
+    assert(ring_index(-4, 2) == 0);
 
-    assert(ring_index(1,2) == 1);
-    assert(ring_index(3,10) == 3);
-    assert(ring_index(11,10) == 1);
-    assert(ring_index(21,10) == 1);
-    
-    assert(ring_index(-1,2) == 1);
-    assert(ring_index(-3,10) == 7);
-    assert(ring_index(-11,10) == 9);
-    assert(ring_index(-21,10) == 9);
+    assert(ring_index(1, 2) == 1);
+    assert(ring_index(3, 10) == 3);
+    assert(ring_index(11, 10) == 1);
+    assert(ring_index(21, 10) == 1);
+
+    assert(ring_index(-1, 2) == 1);
+    assert(ring_index(-3, 10) == 7);
+    assert(ring_index(-11, 10) == 9);
+    assert(ring_index(-21, 10) == 9);
 
     // The assertion itself is not testable, and it shouldn't be tested.
     // Ideally, it would be replaced with tests on the functions that call it
