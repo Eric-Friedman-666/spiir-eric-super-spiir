c9845e47816ca2a0a025e4cd776e85391745e12c
Timothy Davies <timothyfrank.davies@ligo.org>
2024-07-18 02:39:40 +0000

    Upgrade cohfar_assignfar

--- gstlal-spiir/gst/cuda/cohfar/cohfar_assignfar.h
+++ gstlal-spiir/gst/cuda/cohfar/cohfar_assignfar.h
@@ -29,27 +29,10 @@
 #define __COHFAR_ASSIGNFAR_H__
 
 #include <cohfar/background_stats.h>
-#include <ifo_set.h>
-
-// Suppresses a warning that only occurs on NVCC
-// It should be revisited after the gstreamer upgrade
-// See #15
-#if defined(__CUDACC__)
-#pragma diag_suppress 1217
-#endif
 #include <glib.h>
-#if defined(__CUDACC__)
-#pragma diag_default 1217
-#endif
-
-// Suppresses a warning from gstreamer using deprecated mutexes.
-// Should be revisited after the gstreamer upgrade.
-// See #15
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #include <gst/base/gstbasetransform.h>
 #include <gst/gst.h>
-#pragma GCC diagnostic pop
+#include <ifo_set.h>
 
 G_BEGIN_DECLS
 #define COHFAR_ASSIGNFAR_TYPE (cohfar_assignfar_get_type())
@@ -64,7 +47,7 @@ G_BEGIN_DECLS
     (G_type_CHECK_CLASS_TYPE((klass), COHFAR_ASSIGNFAR_TYPE))
 
 typedef struct {
-    GstBaseTransformClass parent_class;
+    GstBaseTransformClass cohfar_assignfar_parent_class;
 } CohfarAssignfarClass;
 
 typedef struct {
