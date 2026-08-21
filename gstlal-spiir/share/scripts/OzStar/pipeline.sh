c69cfd46c9edbc219073d252f5facc5a5a2fc071
Qingyuan Liang <qliang@ozstar.swin.edu.au>
2026-08-21 12:24:11 +1000

    Use standard OzSTAR launcher for crashcar MDC

--- gstlal-spiir/share/scripts/OzStar/pipeline.sh
+++ gstlal-spiir/share/scripts/OzStar/pipeline.sh
@@ -1,45 +1,30 @@
-#!/bin/bash
-#
-#SBATCH --job-name=bbh_inj_rework
-#SBATCH --ntasks=1
-#SBATCH --time=100:00:00
-#SBATCH --mem=12g
-#SBATCH --cpus-per-task=2
-#SBATCH --gres=gpu:1
-#SBATCH --array=13,14,19
-#SBATCH --requeue
 
-SUF=$1
-SUB=$2
-
-cmd () {
+i=$SLURM_ARRAY_TASK_ID
 
 jobno=`seq -f "%03g" ${i} ${i}`
 
-bankdir=/fred/oz996/tdavies/spiir_project/sources/resources/banks/
 
-injs=/fred/oz996/tdavies/spiir_project/sources/resources/injs/gstlal_bbh_astrophysical_imf-1186624850-1187312718.xml.gz
+#bankdir=/fred/oz016/manoj/O4_Banks/O4_New/filt/gstlal_iir_bank_b0optmin97pc_O4a_0
+bankdir=/fred/oz016/sunil/O3b_py2_banks/FB/
+#bankdir=/fred/oz016/sunil/s240422ed/py3-bank
 
-macrostart=1186642720
-# Full test: 1187312718
-macroend=1186650720 #$(($macrostart + 8000))
-noninj_stats_loc=/fred/oz996/tdavies/spiir_project/sources/resources/noninjs # Generated using spiir-O4-EW-Development as of b23a5be5f8f85d62c0b0cdba30fb64560384c853
+#injs=/fred/oz016/sunil/run_utils/injection_files/bbh/SG_RM-1241710700-1242315500.xml
+
+macrostart=1238787954 # Apr 08 2019 19:45:36 UTC
+macroend=1239641219  # Apr 18 2019 16:46:41 UTC
+
+noninj_stats_loc=`pwd`
 inj_stats_loc=`pwd`
 
-map=H1L1_detrsp_map.xml
+map=/fred/oz016/wguo/odds_ratio/O3a/chunk2/multi_det-BNS/H1L1V1_1238787954_detrsp_map.xml
 
-cache=/fred/oz016/data_cleaned/frame.cache.cleaned
-psd=/fred/oz996/tdavies/spiir_project/sources/resources/psd/gstlal_H1L1V1-REFERENCE_PSD-1186624818-687900.xml.gz
-segs=/fred/oz996/tdavies/spiir_project/sources/resources/segs/segments.xml.gz
-vetoes=/fred/oz996/tdavies/spiir_project/sources/resources/vetoes/vetoes.xml.gz
+cache=/fred/oz016/sunil/run_utils/frames_chache/frame_O3a.cache
 
 macronodename=postcohspiir
 
-start=333
+start=0
 bpj=4
 nretry=0
-
-
 
 macrofarinput=${noninj_stats_loc}/${jobno}/${jobno}_marginalized_stats_2w.xml.gz,${noninj_stats_loc}/${jobno}/${jobno}_marginalized_stats_1d.xml.gz,${noninj_stats_loc}/${jobno}/${jobno}_marginalized_stats_2h.xml.gz
 
@@ -49,14 +34,16 @@ macrojobtag=${jobno}
 
 for bank in $(seq -f "%04g" $(( ${start}+${bpj}*${i} )) $(( ${start}+${bpj}*($i) )) ); do
     H1bank=${bankdir}/iir_H1-GSTLAL_SPLIT_BANK_${bank}-a1-0-0.xml.gz
-        L1bank=${bankdir}/iir_L1-GSTLAL_SPLIT_BANK_${bank}-a1-0-0.xml.gz
-        macroiirbank=H1:${H1bank},L1:${L1bank}
+	L1bank=${bankdir}/iir_L1-GSTLAL_SPLIT_BANK_${bank}-a1-0-0.xml.gz
+	V1bank=${bankdir}/iir_V1-GSTLAL_SPLIT_BANK_${bank}-a1-0-0.xml.gz
+	macroiirbank=H1:${H1bank},L1:${L1bank},V1:${V1bank}
 done
 
 for bank in $(seq -f "%04g" $(( ${start}+${bpj}*${i}+1 )) $(( ${start}+${bpj}*($i+1)-1 )) ); do
     H1bank=${bankdir}/iir_H1-GSTLAL_SPLIT_BANK_${bank}-a1-0-0.xml.gz
-        L1bank=${bankdir}/iir_L1-GSTLAL_SPLIT_BANK_${bank}-a1-0-0.xml.gz
-        macroiirbank="${macroiirbank} --iir-bank H1:${H1bank},L1:${L1bank}"
+	L1bank=${bankdir}/iir_L1-GSTLAL_SPLIT_BANK_${bank}-a1-0-0.xml.gz
+	V1bank=${bankdir}/iir_V1-GSTLAL_SPLIT_BANK_${bank}-a1-0-0.xml.gz
+	macroiirbank="${macroiirbank} --iir-bank H1:${H1bank},L1:${L1bank},V1:${V1bank}"
 done
 
 for bank in $(seq -f "%04g" $(( ${start}+${bpj}*${i} )) $(( ${start}+${bpj}*($i) )) ); do
@@ -69,67 +56,8 @@ done
 
 macrooutprefix=${jobno}/${jobno}_zerolag
 
+CMD="$(which gstlal_inspiral_postcohspiir_online) --state-channel-name H1=GDS-CALIB_STATE_VECTOR --state-channel-name L1=GDS-CALIB_STATE_VECTOR --state-channel-name V1=DQ_ANALYSIS_STATE_VECTOR --state-vector-on-bits H1=3 --state-vector-on-bits L1=3 --state-vector-on-bits V1=1027 --state-vector-off-bits H1=0 --state-vector-off-bits L1=0 --state-vector-off-bits V1=0 --job-tag ${macrojobtag} --tmp-space _CONDOR_SCRATCH_DIR --iir-bank ${macroiirbank} --data-source frames --channel-name H1=GDS-CALIB_STRAIN_CLEAN --channel-name L1=GDS-CALIB_STRAIN_CLEAN --channel-name V1=Hrec_hoft_16384Hz --gpu-acc on  --ht-gate-threshold 15.0 --cuda-postcoh-snglsnr-thresh 4 --cuda-postcoh-hist-trials 100 --cuda-postcoh-detrsp-fname ${map} --cuda-postcoh-output-skymap 100 --check-time-stamp --finalsink-output-prefix ${macrooutprefix} --finalsink-snapshot-interval 86400 --cohfar-accumbackground-snapshot-interval 3600 --cohfar-accumbackground-output-prefix ${macrostatsprefix} --cohfar-assignfar-input-fname ${macrofarinput} --cohfar-assignfar-silent-time 0 --cohfar-assignfar-refresh-interval 3600 --finalsink-fapupdater-interval 1800 --finalsink-cluster-window 1 --finalsink-fapupdater-collect-walltime 604800,86400,7200 --finalsink-far-factor 25 --finalsink-gracedb-far-thresh 0.0001 --finalsink-need-online-perform 1 --finalsink-gracedb-group Test --finalsink-gracedb-search MDC --finalsink-gracedb-service-url https://gracedb-playground.ligo.org/api/ --cuda-postcoh-detrsp-refresh-interval 86400 --code-version spiir-O4-EW-development --frame-cache ${cache} --gps-start-time ${macrostart} --gps-end-time ${macroend} --finalsink-singlefar-veto-thresh 0.5 --track-psd ${psd} --psd-fft-length 4 --finalsink-fapupdater-output-fname ${macrolocfapoutput}"
+CMD="${CMD} --single-background-read-dir ${noninj_stats_loc}/${jobno}"
+CMD="${CMD} --single-background-write-dir ${inj_stats_loc}/${jobno}"
 
-CMD="$(which gstlal_inspiral_postcohspiir_online) \
-        --job-tag ${macrojobtag} \
-        --iir-bank ${macroiirbank} \
-        --data-source frames \
-        --frame-cache ${cache} \
-        --gps-start-time ${macrostart} \
-        --gps-end-time ${macroend} \
-        --reference-psd ${psd} \
-        --channel-name H1=DCH-CLEAN_STRAIN_C02 \
-        --channel-name L1=DCH-CLEAN_STRAIN_C02 \
-        --cohfar-accumbackground-output-prefix ${macrostatsprefix} \
-        --cohfar-accumbackground-snapshot-interval 3600 \
-        --cohfar-assignfar-silent-time 0 \
-        --cohfar-assignfar-input-fname ${macrofarinput} \
-        --cohfar-assignfar-refresh-interval 3600 \
-        --gpu-acc on \
-        --ht-gate-threshold 15.0 \
-        --cuda-postcoh-snglsnr-thresh 4 \
-        --cuda-postcoh-hist-trials 100 \
-        --cuda-postcoh-detrsp-fname ${map} \
-        --cuda-postcoh-detrsp-refresh-interval 86400 \
-        --cuda-postcoh-output-skymap 100 \
-        --check-time-stamp \
-        --finalsink-fapupdater-collect-walltime 604800,86400,7200 \
-        --finalsink-fapupdater-interval 1800 \
-        --finalsink-output-prefix ${macrooutprefix} \
-        --finalsink-snapshot-interval 86400 \
-        --finalsink-cluster-window 1 \
-        --finalsink-far-factor 2 \
-        --finalsink-singlefar-veto-thresh 0.5 \
-        --finalsink-superevent-thresh 0.0001 \
-        --finalsink-need-online-perform 0 \
-        --finalsink-gracedb-far-threshold 0.0001 \
-        --code-version spiir-review-O3-EW \
-        --psd-fft-length 32 \
-        --injection-file ${injs} \
-        --frame-segments-file ${segs} \
-        --frame-segments-name datasegments \
-        --veto-segments-file ${vetoes} \
-        --finalsink-gracedb-group Test \
-        --finalsink-gracedb-search MDC \
-        --finalsink-gracedb-service-url https://gracedb-playground.ligo.org/api/ \
-        --finalsink-fapupdater-output-fname ${macrolocfapoutput} \
-        "
-}
-
-if [ $SUB -eq 1 ]
-then
-  i=$SLURM_ARRAY_TASK_ID
-  cmd
-  srun $CMD
-else
-  echo "Loop"
-  arrays=$(grep -m 1 "SBATCH --array" pipeline.sh | sed 's/.*=//' | sed 's/-/\ /')
-  for TID in ${arrays//,/ }; do
-    i=$TID
-    echo "Run: "$i
-    cmd
-    echo $CMD
-    $CMD > logs_${SUF}/pipe_${TID}.out 2>logs_${SUF}/pipe_${TID}.err
-    #disown
-  done
-fi
+$CMD
---
+++ gstlal-spiir/share/scripts/OzStar/submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=ck2-multi_det-BNS
+#SBATCH --ntasks=1
+#SBATCH --time=168:00:00
+#SBATCH --mem=14g
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --array=0-24
+#SBATCH --requeue
+#SBATCH -o logs/pipe_%A_%a.out # File to which STDOUT will be written
+#SBATCH -e logs/pipe_%A_%a.err # File to which STDERR will be written
+
+source /fred/oz016/gwdc_spiir_pipeline_codebase/scripts_n_things/build/bash_helper_functions.sh
+# run_spiir -e SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID schoudhary_jan_2024 bash pipeline.sh
+# run_spiir -e SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID manoj__spiir-O4-EW-development bash pipeline.sh
+run_spiir_py3 -e SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID qliang-Eric-crashcar-mdc bash pipeline.sh
